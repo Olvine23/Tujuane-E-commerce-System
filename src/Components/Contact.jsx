fe398d0e545bf00a6ca75d28b37314716e8528a4
ClarenceLaria <clarencelaria@gmail.com>
2023-10-26 15:16:50 +0300

    responsiveness laria

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,11 +7,6 @@ function Contact (){
   return (
     
     <div className='contactPage'>
-        <div className='aBriefDescription'>
-          {/* <h1>Logo</h1> */}
-          {/* <img src="https://media.licdn.com/dms/image/C5603AQG60DA1x_LR6w/profile-displayphoto-shrink_800_800/0/1646254727260?e=2147483647&v=beta&t=mynJ-MlRPeQnmmbtXHjBNq8saDTzPU38UNqgngckQaE" alt="" /> */}
-        <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo quam suscipit aut dolores veritatis et incidunt eaque, distinctio iure eum. Magni eaque modi magnam laborum, voluptate explicabo aperiam laboriosam nostrum?</p>
-        </div>
         <div className='contacts'>
         <h3> Contacts Info </h3>     
         <p>
@@ -19,9 +14,6 @@ function Contact (){
           Email :  tujuaneadventures@gmail.com <br/>
 
         </p>
-        
-        
-      
        </div>
         <div className='social'>
               <h3>Socials</h3>
@@ -38,8 +30,6 @@ function Contact (){
                   <img  src="/images/nobgtiktok.png" alt="" />
                   
                 </div>
-                
-              
               </div>  
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo quam suscipit aut dolores veritatis </p>
        </div>
